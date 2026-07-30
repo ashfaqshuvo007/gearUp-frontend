746b63e99e59e12140c69ac8e4410d437dce7b57
Ashfaq Hussain Ahmed <ashfaqshuvo14@gmail.com>
2026-07-30 23:27:02 +0600

    Proxy middleware. Access to hidden resources based on role

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,23 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import jwt from "jsonwebtoken";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const verifyToken = (token: string, secret: string) => {
+  try {
+    const verifiedToken = jwt.verify(token, secret);
+    return {
+      success: true,
+      data: verifiedToken,
+    };
+  } catch (error: any) {
+    console.error("Token Verification Failed!", error);
+    return {
+      success: false,
+      error: (error as Error).message,
+    };
+  }
+};
